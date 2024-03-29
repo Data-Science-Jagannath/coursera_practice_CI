8af8684d2444270e1302726616f94ec9a3baff91
Data-Science-Jagannath <157683814+Data-Science-Jagannath@users.noreply.github.com>
2024-03-29 06:21:28 +0000

    adding initial layout of python project

---
+++ hello.py
@@ -0,0 +1,16 @@
+def more_hello():
+    return "hi"
+
+
+def more_bye():
+    return "bye"
+
+
+# this is nonsense code that generated a warning in when we write
+# make lint in cmd
+
+var = 1
+var = var
+
+# bad syntax
+# foo =
